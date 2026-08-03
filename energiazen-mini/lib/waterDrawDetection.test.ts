2aa745b0528d3e67a264d4e6961ba2314a5dde6d
Calvonen <marko.raitanen@gmail.com>
2026-08-03 20:14:01 +0300

    Muuta vedenottotunnistuksen raja viiteen asteeseen

--- energiazen-mini/lib/waterDrawDetection.test.ts
+++ energiazen-mini/lib/waterDrawDetection.test.ts
@@ -23,22 +23,22 @@ export function runWaterDrawDetectionUnitTests() {
     "yksittainen lukema ei riita havaintoon",
   );
   assertEqual(
-    detectsWaterDraw([sample(0, 15.7), sample(2, 12.2)]),
+    detectsWaterDraw([sample(0, 15.7), sample(2, 10.7)]),
     true,
     "nopea vahintaan kynnysarvon pudotus ikkunan sisalla havaitaan",
   );
   assertEqual(
-    detectsWaterDraw([sample(0, 15.7), sample(2, 14.0)]),
+    detectsWaterDraw([sample(0, 15.7), sample(2, 10.8)]),
     false,
     "alle kynnysarvon pudotus ei laukea",
   );
   assertEqual(
-    detectsWaterDraw([sample(0, 15.7), sample(10, 12.2)]),
+    detectsWaterDraw([sample(0, 15.7), sample(10, 10.7)]),
     false,
     "yli ikkunan (5 min) tapahtuva pudotus ei laukea",
   );
   assertEqual(
-    detectsWaterDraw([sample(0, 12.2), sample(2, 15.7)]),
+    detectsWaterDraw([sample(0, 10.7), sample(2, 15.7)]),
     false,
     "nousu ei laukea, vain pudotus",
   );
@@ -48,9 +48,9 @@ export function runWaterDrawDetectionUnitTests() {
     "puuttuvat lukemat eivat itsessaan laukea eivatka riko vertailua",
   );
   assertEqual(
-    detectsWaterDraw([sample(0, 15.7), sample(2, 14.0), sample(4, 12.2)]),
+    detectsWaterDraw([sample(0, 15.7), sample(2, 13.0), sample(4, 10.7)]),
     true,
     "vertailu ulottuu koko liukuvan ikkunan yli, ei vain edelliseen lukemaan - " +
-      "kumpikaan yksittainen askel (1.7 ja 1.8) ei ylita kynnysta, mutta 0-4 min valinen 3.5 asteen pudotus ylittaa",
+      "kumpikaan yksittainen askel (2.7 ja 2.3) ei ylita kynnysta, mutta 0-4 min valinen 5.0 asteen pudotus saavuttaa sen",
   );
 }
